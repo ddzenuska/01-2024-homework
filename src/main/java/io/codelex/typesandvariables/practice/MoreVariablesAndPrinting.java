cb227ba3420561cf3502eb4cbce68bea8a33295c
ddzenuska <ddzenuska@gmail.com>
2024-03-04 12:01:50 +0200

    Finished task MoreVariablesAndPrinting

--- src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
+++ src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
@@ -1,26 +1,32 @@
 package io.codelex.typesandvariables.practice;
 
+import java.text.DecimalFormat;
 public class MoreVariablesAndPrinting {
     public static void main(String[] args) {
-        String myName, myEyes, myTeeth, myHair;
-        int myAge, myHeight, myWeight;
+        String PersName, PersEyeColor, PersTeethColor, PersHairColor;
+        int PersAge, PersHeightInch, PersWeightLbs;
+        double WeightInKg, HeightInCm;
 
-        myName = "Zed A. Shaw";
-        myAge = 35;
-        myHeight = 74;  // inches
-        myWeight = 180; // lbs
-        myEyes = "Blue";
-        myTeeth = "White";
-        myHair = "Brown";
+        DecimalFormat df = new DecimalFormat("0.00");
 
-        System.out.println("Let's talk about " + myName + ".");
-        System.out.println("He's " + myHeight + " inches tall.");
-        System.out.println("He's " + myWeight + " pounds heavy.");
+        PersName = "Zed A. Shaw";
+        PersAge = 35;
+        PersHeightInch = 74;  //height in inches
+        PersWeightLbs = 180; // weight in lbs
+        HeightInCm = PersHeightInch * 2.54;  // converting height in centimeters
+        WeightInKg = PersWeightLbs * 0.453592; // converting weight in kilograms
+        PersEyeColor = "blue";
+        PersTeethColor = "white";
+        PersHairColor = "brown";
+
+        System.out.println("Let's talk about " + PersName + ".");
+        System.out.println("He's " + PersHeightInch + " inches tall, which in metrics is " + df.format(HeightInCm) + " centimeters.");
+        System.out.println("He's " + PersWeightLbs + " pounds heavy or " + df.format(WeightInKg) + " kilograms.");
         System.out.println("Actually, that's not too heavy.");
-        System.out.println("He's got " + myEyes + " eyes and " + myHair + " hair.");
-        System.out.println("His teeth are usually " + myTeeth + " depending on the coffee.");
+        System.out.println("He's got " + PersEyeColor + " eyes and " + PersHairColor + " hair.");
+        System.out.println("His teeth are usually " + PersTeethColor + " depending on the coffee.");
 
-        System.out.println("If I add " + myAge + ", " + myHeight + ", and " + myWeight
-                + " I get " + (myAge + myHeight + myWeight) + ".");
+        System.out.println("If I add " + PersAge + ", " + PersHeightInch + ", and " + PersWeightLbs
+                + " I get " + (PersAge + PersHeightInch + PersWeightLbs) + ".");
     }
 }
