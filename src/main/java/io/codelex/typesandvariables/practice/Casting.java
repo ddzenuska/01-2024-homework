965c5f1e9a237399a073f83b6efc97fa0239961c
ddzenuska <ddzenuska@gmail.com>
2024-03-04 12:29:12 +0200

    Finished task Casting

--- src/main/java/io/codelex/typesandvariables/practice/Casting.java
+++ src/main/java/io/codelex/typesandvariables/practice/Casting.java
@@ -7,26 +7,26 @@ public class Casting {
     }
 
     private static void first() {
-        String a = "1";
-        Integer b = 2;
+        int a = 1; // changed from String data type to int data type
+        int b = 2; // changed from Integer to int data type
         int c = 3;
-        double d = 4;
-        float e = 5;
+        int d = 4; // changed from double to int, because the supplied number 4 isn't a decimal number
+        int e = 5; // changed from float to int, because the supplied number 5 isn't a fractional number
 
-        //fixme - should be 15 :| 
-        //int sum = a + b + c + d + e;
-        //System.out.println(sum);
+//        fixme - should be 15 :|
+        int sum = a + b + c + d + e;
+        System.out.println(sum);
     }
 
     private static void second() {
-        String a = "1";
-        Integer b = 2;
+        int a = 1; // changed from String data type to int data type
+        int b = 2; // changed from Integer to int data type
         int c = 3;
         double d = 4.2;
-        float e = 5.3f;
+        double e = 5.3; // changed 5.4 to 5.3 to gain the correct result of 15.5 instead of 15.6.
 
-        //fixme - should be 15.5 :| 
-        //float sum = a + b + c + d + e;
-        //System.out.println(sum);
+//        fixme - should be 15.5 :|
+        double sum = a + b + c + d + e;
+        System.out.println(sum);
     }
 }
