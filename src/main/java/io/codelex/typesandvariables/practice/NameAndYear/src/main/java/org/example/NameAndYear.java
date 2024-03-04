9493d9887a91b212c8661d4859fbc8361e38a4ce
ddzenuska <ddzenuska@gmail.com>
2024-03-04 13:02:54 +0200

    Completed task four, called NameAndYear

---
+++ src/main/java/io/codelex/typesandvariables/practice/NameAndYear/src/main/java/org/example/NameAndYear.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+public class NameAndYear {
+        String name;
+        int yearOfBirth;
+
+    public NameAndYear(String myName, int myBirthYear) { // constructor method for creating a new object - person
+        name = myName;
+        yearOfBirth = myBirthYear;
+    }
+
+    public void introduce() { // method that outputs the name and year of birth of the created person
+        System.out.println("My name is " + name + " and I was born in " + yearOfBirth + ".");
+
+    }
+
+    public static void main(String[] args) {
+
+        NameAndYear juanValdez = new NameAndYear("Juan Valdez", 1995); // creating a new instance
+
+        juanValdez.introduce(); // calling the introduce method to print the objects values in the terminal
+
+        }
+    }
