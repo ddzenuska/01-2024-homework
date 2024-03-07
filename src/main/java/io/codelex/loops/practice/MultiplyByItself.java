d60ac244139a6ac3f63d0f84404caf635987b8b6
ddzenuska <ddzenuska@gmail.com>
2024-03-07 16:11:59 +0200

    Loops ex.2 - MultiplyByItself

--- src/main/java/io/codelex/loops/practice/MultiplyByItself.java
+++ src/main/java/io/codelex/loops/practice/MultiplyByItself.java
@@ -7,16 +7,20 @@ public class MultiplyByItself {
     public static void main(String[] args) {
         int i, n;
 
-        System.out.print("Input number of terms : ");
+        System.out.print("Input number of terms: ");
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
-
-        /*
-        todo - complete loop to multiply i with itself n times, it is NOT allowed to use Math.pow()
-        for (.......) {
-          System.out.println(........);
+        int result = 0;
+        for (i = 1; i <= n; i++) {
+            result = 1;
+            for (int j = 1; j <= n; j++) {
+                /* for every "i" iteration, "j" iterates fully, multiplying itself (current
+                value) by 1. The value grows with every iteration.*/
+                result *= i;
+            }
         }
-        */
+        System.out.println("\n\t" + (i - 1) + "^" + n + " = " + result + " "); // prints the full result, instead of value on each iteration.
+        // wrote (i - 1) so the input value is printed correctly, instead of being larger by one.
     }
 
 }
