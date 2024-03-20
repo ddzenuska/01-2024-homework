7e90b3d481ac1d428bacac8333e29e4a08d58acf
ddzenuska <ddzenuska@gmail.com>
2024-03-20 13:06:51 +0200

    Exceptions ex.2

--- src/main/java/io/codelex/exceptions/practice/Exercise2.java
+++ src/main/java/io/codelex/exceptions/practice/Exercise2.java
@@ -2,11 +2,28 @@ package io.codelex.exceptions.practice;
 
 public class Exercise2 {
     public void methodA() throws ArithmeticException {
+        try {
+            methodB();
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
     }
 
     public void methodB() throws ArithmeticException {
+        try {
+            methodC();
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 
     public void methodC() throws ArithmeticException {
+        try {
+            System.out.println(10 / 0);
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
