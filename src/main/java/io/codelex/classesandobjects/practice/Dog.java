26a3a673864880adf684f6207ebe48c3e759cc04
ddzenuska <ddzenuska@gmail.com>
2024-03-25 13:46:55 +0200

    Dog / Dogtest fixed

--- src/main/java/io/codelex/classesandobjects/practice/Dog.java
+++ src/main/java/io/codelex/classesandobjects/practice/Dog.java
@@ -39,30 +39,3 @@ public class Dog {
                 ", father: " + (this.father != null ? this.father.name() : "Unknown") + ".";
     }
 }
-
-class Dogtest {
-    public static void main(String[] args) {
-        Dog max = new Dog("Max", "male");
-        Dog rocky = new Dog("Rocky", "male");
-        Dog sparky = new Dog("Sparky", "male");
-        Dog buster = new Dog("Buster", "male");
-        Dog sam = new Dog("Sam", "male");
-        Dog lady = new Dog("Lady", "female");
-        Dog molly = new Dog("Molly", "female");
-        Dog coco = new Dog("Coco", "female");
-
-        // adding parents
-        max = new Dog("Max", "male", lady, rocky);
-        rocky = new Dog("Rocky", "male", molly, buster);
-        buster = new Dog("Buster", "male", molly, sam);
-        coco = new Dog("Coco", "female", lady, sparky);
-
-        System.out.println(max);
-        System.out.println(sparky);
-        System.out.println(coco);
-
-        // finding father name and if dogs have the same mother.
-        System.out.println(max.fathersName());
-        System.out.println(max.hasSameMotherAs(rocky));
-    }
-}
