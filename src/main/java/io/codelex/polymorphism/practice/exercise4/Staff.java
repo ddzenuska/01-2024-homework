9416b20f2007a9b2d2b0ade261c545d07b023dab
ddzenuska <ddzenuska@gmail.com>
2024-04-04 16:07:02 +0300

    Ex. 4

--- src/main/java/io/codelex/polymorphism/practice/exercise4/Staff.java
+++ src/main/java/io/codelex/polymorphism/practice/exercise4/Staff.java
@@ -7,7 +7,7 @@ public class Staff {
     // Sets up the list of staff members.
     //-----------------------------------------------------------------
     public Staff() {
-        staffList = new StaffMember[6];
+        staffList = new StaffMember[8];
         staffList[0] = new Executive("Sam", "123 Main Line",
                 "555-0469", "123-45-6789", 2423.07);
         staffList[1] = new Employee("Carla", "456 Off Line",
@@ -20,8 +20,19 @@ public class Staff {
                 "555-8374");
         staffList[5] = new Volunteer("Cliff", "321 Duds Lane",
                 "555-7282");
+        staffList[6] = new Commission("Karl", "72 Long Lane",
+                "555-7282", "821-21-3780", 6.25, .2);
+        staffList[7] = new Commission("Lizzy", "278 Flower St.",
+                "555-7282", "064-56-4650", 9.75, .15);
+
         ((Executive) staffList[0]).awardBonus(500.00);
         ((Hourly) staffList[3]).addHours(40);
+
+        // Editing Commission staff objects
+        ((Commission) staffList[6]).addHours(35);
+        ((Commission) staffList[6]).addSales(400);
+        ((Commission) staffList[7]).addHours(40);
+        ((Commission) staffList[7]).addSales(950);
     }
 
     //-----------------------------------------------------------------
@@ -29,9 +40,9 @@ public class Staff {
     //-----------------------------------------------------------------
     public void payday() {
         double amount;
-        for (int count = 0; count < staffList.length; count++) {
-            System.out.println(staffList[count]);
-            amount = staffList[count].pay(); // polymorphic
+        for (StaffMember staffMember : staffList) {
+            System.out.println(staffMember);
+            amount = staffMember.pay(); // polymorphic
             if (amount == 0.0)
                 System.out.println("Thanks!");
             else
