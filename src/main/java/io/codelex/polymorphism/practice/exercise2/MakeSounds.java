28efec561f0ff33400e9556282370fdcbacde42b
ddzenuska <ddzenuska@gmail.com>
2024-04-04 14:02:14 +0300

    Ex. 2

--- src/main/java/io/codelex/polymorphism/practice/exercise2/MakeSounds.java
+++ src/main/java/io/codelex/polymorphism/practice/exercise2/MakeSounds.java
@@ -1,7 +1,15 @@
 package io.codelex.polymorphism.practice.exercise2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MakeSounds {
     public static void main(String[] arg) {
-        
+        List<Sound> things = new ArrayList<>();
+        things.add(new Firework());
+        things.add(new Parrot());
+        things.add(new Radio());
+
+        things.forEach(Sound::playSound); // used method reference for a shorter code, instead of a for loop / lambda
     }
 }
