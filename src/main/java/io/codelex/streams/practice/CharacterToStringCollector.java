eef7dc402fc8e48b5af5a641dab010fefaa8dc1f
ddzenuska <ddzenuska@gmail.com>
2024-04-13 13:09:48 +0300

    CharacterToStringCollector

--- src/main/java/io/codelex/streams/practice/CharacterToStringCollector.java
+++ src/main/java/io/codelex/streams/practice/CharacterToStringCollector.java
@@ -1,5 +1,6 @@
 package io.codelex.streams.practice;
 
+import java.util.Collections;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
@@ -10,26 +11,27 @@ import java.util.stream.Collector;
 public class CharacterToStringCollector implements Collector<Character, StringBuilder, String> {
     @Override
     public Supplier<StringBuilder> supplier() {
-        return null;
+        return StringBuilder::new;
     }
 
     @Override
-    public BiConsumer<StringBuilder, Character> accumulator() {
-        return null;
+    public BiConsumer<StringBuilder, Character> accumulator() { //adds elements to a result container StringBuilder
+        return StringBuilder::append;
     }
 
     @Override
-    public BinaryOperator<StringBuilder> combiner() {
-        return null;
+    public BinaryOperator<StringBuilder> combiner() { //merge two result containers
+        return StringBuilder::append;
     }
 
     @Override
-    public Function<StringBuilder, String> finisher() {
-        return null;
+    public Function<StringBuilder, String> finisher() { //transforms to the final result - a string
+        return StringBuilder::toString;
     }
 
     @Override
-    public Set<Characteristics> characteristics() {
-        return null;
+    public Set<Characteristics> characteristics() { //returns a set of characteristics of this Collector,
+        // UNORDERED in this case
+        return Collections.singleton(Characteristics.UNORDERED);
     }
 }
